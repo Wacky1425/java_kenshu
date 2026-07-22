9d179488596a53507b3019e8a4d7b517d756e1ca
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-22 12:30:31 +0900

    q0006

---
+++ src/wakitahokuto_kenshu/Q0006.java
@@ -0,0 +1,17 @@
+package wakitahokuto_kenshu;
+
+//javaが用意しているutilパッケージにあるCalendarクラスを使えるようにする
+import java.util.Calendar;
+
+public class Q0006 {
+
+	public static void main(String[] args) {
+		
+		//CalendarクラスのインスタンスnowDateをCalendarクラスにあるgetInstanceメソッドを使って作成する。
+		Calendar nowDate = Calendar.getInstance();
+		
+		//nowDateインスタンスに現在時刻を取得するメソッドgetTime()を使用して時間を取得し、それを出力。
+		System.out.println(nowDate.getTime());
+	}
+
+}
