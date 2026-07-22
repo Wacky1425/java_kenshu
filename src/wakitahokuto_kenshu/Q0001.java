daea4fd0a19cc11567653925cf83c0af8f743f32
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-22 09:38:55 +0900

    first commit

---
+++ src/wakitahokuto_kenshu/Q0001.java
@@ -0,0 +1,24 @@
+//wakitahokuto_kenshuというパッケージ(グループ)を作成する宣言
+package wakitahokuto_kenshu;
+
+//他の全てのクラス、パッケージから参照可能なクラスQ0001を作成する
+public class Q0001 {
+	
+//
+	public static void main(String[] args) {
+		
+		// 
+		System.out.println("Hello World");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/wakitahokuto_kenshu/Q0002.java
@@ -0,0 +1,10 @@
+package wakitahokuto_kenshu;
+
+public class Q0002 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
