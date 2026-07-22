34faf05d86851b7dbab1b9c1ddf2195f1d0a083a
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-22 11:48:32 +0900

    q0005

---
+++ src/wakitahokuto_kenshu/Q0005.java
@@ -0,0 +1,17 @@
+package wakitahokuto_kenshu;
+
+public class Q0005 {
+
+	public static void main(String[] args) {
+		
+		//整数型の変数iを初期値1として宣言し、{}内の処理を一度実行するごとにiを+1していき、i<=100を満たす限り繰り返し実行する
+		for(int i=1;i<=100;i++) {
+			
+			//"Hello World"の文字列と"空白"と変数"i"と"回目"という文字列を連結して1つの文字列として出力する
+			System.out.println("Hello World"+" "+i+"回目");
+			
+			
+		}
+	}
+
+}
