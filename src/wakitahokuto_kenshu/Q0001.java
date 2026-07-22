499dd31dbb1bf48cc374afa70e4bc8e2eb1dc416
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-22 10:51:28 +0900

    q0001test

--- src/wakitahokuto_kenshu/Q0001.java
+++ src/wakitahokuto_kenshu/Q0001.java
@@ -1,13 +1,13 @@
 //wakitahokuto_kenshuというパッケージ(グループ)を作成する宣言
 package wakitahokuto_kenshu;
 
-//他の全てのクラス、パッケージから参照可能なクラスQ0001を作成する
+//他のクラス、パッケージから参照可能なクラスQ0001を作成する
 public class Q0001 {
 	
-//
+//どこからでもアクセス可能な、クラスQ0001自体に属する、戻り値のない、引数をString型の配列(args)で受け取る、最初に呼ばれるメソッドを作成
 	public static void main(String[] args) {
 		
-		// 
+		// systemクラスの標準出力を示すフィールドであるoutが指すprintlnメソッドを呼び出して、()内の文字列"Hello World" を表示し改行する
 		System.out.println("Hello World");
 		
 		
