612b09f141c554ad0f51c61584a538dc95453362
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-22 15:13:57 +0900

    q0001_ver1

---
+++ src/wakitahokuto_kenshu/Q0011.java
@@ -0,0 +1,19 @@
+package wakitahokuto_kenshu;
+
+public class Q0011 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//整数型の変数としてsum、a、bを宣言し、初期値10,15も設定する。
+		int sum,dif,pro,quo,a=10,b=15;
+		
+		//変数sumにaとbの和を代入する。
+		sum = a + b;
+		
+		//変数sumの値を標準出力する。
+		System.out.println(sum);
+
+	}
+
+}
