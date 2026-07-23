fbd8d3956dde3d152e0fb54f5e67e098c0aab91d
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-23 17:17:20 +0900

    q0016test

---
+++ bin/Q0015.java
@@ -0,0 +1,25 @@
+package wakitahokuto_kenshu;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Q0015 {
+
+	public static void main(String[] args) {
+
+		String path = "Z:\\個人フォルダ\\わ行\\脇田北斗\\01.研修資料\\05.Java研修\\jusho.csv";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line;
+            while ((line = br.readLine()) != null) {
+               
+                System.out.println(line);
+            }
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/wakitahokuto_kenshu/Q0016.java
+++ src/wakitahokuto_kenshu/Q0016.java
@@ -4,25 +4,23 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Q0015 {
+public class Q0016 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
 		String path = "Z:\\個人フォルダ\\わ行\\脇田北斗\\01.研修資料\\05.Java研修\\jusho.csv";
-		
 		
 		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line;
             while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                for (String val : values) {
-                    System.out.print(val + "\t");
-                }
-                System.out.println();
+            	String[] split = line.split(",");
+            	if(split[4].equals("0")) {
+            		System.out.println(line);
+            	}
+            	
             }
 		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
+
 			e.printStackTrace();
 		}
 	}
