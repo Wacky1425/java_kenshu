cc6feb32c6a1a2202d2f80f20310f0d461bc4e0f
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-24 15:39:36 +0900

    q0001end0

--- bin/Q0021.java
+++ bin/Q0021.java
@@ -1,90 +1,154 @@
+
 package wakitahokuto_kenshu;
+
 
 import java.util.Random;
 
+
 public class Q0021 {
 
+	
 	public static void main(String[] args) {
 		
 		
-		int a_flag = 0;
+		int[] a_flag = {0};
 		
 		
-		for(int i = 1;i<=10 && a_flag<3;i++) {
+		for(int i = 1;i<=10 && a_flag[0]<3;i++) {
+			
+			
 			Random rand = new Random();
+			
+			
 			int a=rand.nextInt(3);
+			
+			
 			int b=rand.nextInt(3);
+			
+			
 			boolean aiko_flag = false;
 			
 			
-			
 			System.out.println(i + "回目" + " Aさん" + hand(a) + " Bさん" + hand(b) + " 勝者：" + winner(a,b,a_flag,aiko_flag));
-			
-			
-			
-		}
-		
-		
-			
-			
-		
-		
-		
+		}	
 	}
 	
 	//ハンドが何にあたるかを文字として返す関数
 	private static String hand(int a) {
+		
+		
 		String ans = null;
+		
+		
 		switch (a) {
+		
+		
 		case 0:
-			ans = "パー";
-			break;
-		case 1:
-			ans = "チョキ";
-			break;
-		case 2:
-			ans = "グー";
 			
+			
+			ans = "パー";
+			
+			
+			break;
+			
+			
+		case 1:
+			
+			
+			ans = "チョキ";
+			
+			
+			break;
+			
+			
+		case 2:
+			
+			
+			ans = "グー";
 		}
+		
+		
 		return ans;
 	}
 			
 	//勝者を返す関数
-	private static String winner(int a, int b, int a_flag, boolean aiko_flag) {
+	private static String winner(int a, int b, int[] a_flag, boolean aiko_flag) {
 		
-				this.a_flag = a_flag;
+		
 				switch(Math.abs(a-b)) {
+				
+				
 					case 1:
+						
+						
 						if(a>b) {
-							a_flag = 0;
+							
+							
+							a_flag[0] = 0;
+							
+							
 						}else {
-							a_flag++;
+							
+							
+							a_flag[0]++;
+							
+							
 						}
+						
+						
 						break;
+						
+						
 					case 0:
-						a_flag = 0;
+						
+						
+						a_flag[0] = 0;
+						
+						
 						aiko_flag = true;
+						
+						
 						break;
+						
+						
 					case 2:
+						
+						
 						if(a>b) {
-							a_flag++;
+							
+							
+							a_flag[0]++;
+							
+							
 						}else {
-							a_flag = 0;
+							
+							
+							a_flag[0] = 0;
 						}
 				}
+				
 				
 				String winner;
 				
 				
 				if(aiko_flag) {
+					
+					
 					winner = "なし";
 					
-				}else if(a_flag == 0){
+					
+				}else if(a_flag[0] == 0){
+					
+					
 					winner = "Aさん";
+					
 									
 				}else {
+					
+					
 					winner = "Bさん";
 				}
+				
 				
 				return winner;
 	}
