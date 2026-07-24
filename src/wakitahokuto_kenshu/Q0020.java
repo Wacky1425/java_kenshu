e01400ceb0bbed7ed716aa5e0e0878368097522f
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-24 14:16:55 +0900

    q0020test and q0001ver.3

--- src/wakitahokuto_kenshu/Q0020.java
+++ src/wakitahokuto_kenshu/Q0020.java
@@ -7,21 +7,14 @@ public class Q0020 {
 	public static void main(String[] args) {
 		
 		
-		
-		int gu = 2;
-		int cho = 1;
-		int pa = 0;
-		
 		int a_flag = 0;
 		
 		
-		for(int i = 1;i<=10;i++) {
+		for(int i = 1;i<=10 && a_flag<3;i++) {
 			Random rand = new Random();
 			int a=rand.nextInt(3);
 			int b=rand.nextInt(3);
 			boolean aiko_flag = false;
-			
-			System.out.println(a + ","+ b);
 			
 			switch(Math.abs(a-b)) {
 				case 1:
@@ -43,10 +36,7 @@ public class Q0020 {
 					}
 			}
 			
-			String a_hand, b_hand, winner;
-			
-			a_hand = "";
-			b_hand = "";
+			String winner;
 			
 			
 			if(aiko_flag) {
@@ -59,20 +49,34 @@ public class Q0020 {
 				winner = "Bさん";
 			}
 			
-			System.out.println(i + "回目" + " Aさん" + a_hand + " Bさん" + b_hand + " 勝者：" + winner);
+			System.out.println(i + "回目" + " Aさん" + hand(a) + " Bさん" + hand(b) + " 勝者：" + winner);
 			
 			
 			
 		}
 		
-		//ハンドが何にあたるかを文字として返す関数
-//		private static String hand() {
-//			switch 
-//		}
+		
 			
 			
 		
 		
 		
 	}
+	
+	//ハンドが何にあたるかを文字として返す関数
+			private static String hand(int a) {
+				String ans = null;
+				switch (a) {
+				case 0:
+					ans = "パー";
+					break;
+				case 1:
+					ans = "チョキ";
+					break;
+				case 2:
+					ans = "グー";
+					
+				}
+				return ans;
+			}
 }
