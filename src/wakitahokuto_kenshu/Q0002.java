de945ff7501e8c03c8b51ede9de29a75ef683f36
wacky1425 <wackyhokuto.1425@gmail.com>
2026-07-27 15:59:45 +0900

    q0004ver.2

--- src/wakitahokuto_kenshu/Q0002.java
+++ src/wakitahokuto_kenshu/Q0002.java
@@ -7,7 +7,7 @@ public class Q0002 {
 	//どこからでもアクセス可能な、Q0002自体に属する、処理完了後に呼び出し元に渡す結果やデータがない、入力値を要素が文字列の配列(args)で受け取る、Q0002が呼ばれたときに最初に呼ばれる処理
 	public static void main(String[] args) {
 		
-		//javaに暗黙的に実装されている基本的な処理群の入出力およびエラー出力に関する処理群の内、出力属性の群に対して、HelloWorldをコンソールへ出力して改行する処理を二回行う。
+		//javaに実装されている入出力およびエラー出力に関する処理群の中の、出力にあたる変数が指す処理として、HelloWorldをコンソールへ出力して改行する処理を二回行う。
 		System.out.println("Hello World");
 		System.out.println("Hello World");
 		
